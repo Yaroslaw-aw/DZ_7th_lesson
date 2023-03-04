d9d5276d45bd34d4804eeb9b580fc8cf278589b6
Yaroslaw-aw <manumission@mail.ru>
2023-03-04 18:53:31 +0300

    Task 52 ready

--- Program.cs
+++ Program.cs
@@ -13,7 +13,47 @@ namespace DZ_7th_lesson
     {
         static void Main(string[] args)
         {
+            int m = new Random().Next(3,5);
+            int n = new Random().Next(3, 5);
+
             int[,] dz_array = Fill_Array(m, n);
+
+            Average_of_columns(dz_array);
+
+
+            void Average_of_columns(int[,] _array)
+            {
+                double average = 0;
+                double sum = 0;
+
+                for (int i = 0; i < _array.GetLength(0); i++)
+                {
+                    sum = 0;
+
+                    for (int j = 0; j < _array.GetLength(1); j++)
+                    {
+                        sum += _array[j, i];                        
+                    }
+                    average = sum / _array.GetLength(0);
+                    Console.Write(average + " ");
+                }
+            }
+
+            int[,] Fill_Array(int m, int n)
+            {
+                int[,] _array = new int[m, n];
+
+                for (int i = 0; i < _array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < _array.GetLength(1); j++)
+                    {
+                        _array[i, j] = new Random().Next(10);
+                        Console.Write($"{_array[i, j]} ");
+                    }
+                    Console.WriteLine();
+                }
+                return _array;
+            }
         }
     }
 }
